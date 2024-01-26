c4448ca832e7f082ea5ce3336634fbddaa5ad778
George Sutton <46813782+georgeosutton@users.noreply.github.com>
2024-01-26 10:38:42 +0000

    chore: remove page revalidate

--- app/(www)/[[...slug]]/page.tsx
+++ app/(www)/[[...slug]]/page.tsx
@@ -1,11 +1,8 @@
 import { loadPage } from "@/sanity/loader/loadQuery";
 import { notFound } from "next/navigation";
-import NextLink from "next/link";
 import { draftMode } from "next/headers";
 import PagePreview from "@/app/components/page/PreviewPage";
 import Page from "@/app/components/page/Page";
-
-export const revalidate = 90;
 
 export default async function PageRoute({
   params,
