33714d67ee61434045bda7bc12ed98019e616e17
George Sutton <46813782+georgeosutton@users.noreply.github.com>
2024-04-28 18:20:44 +0100

    feat: turn overlay off if outside of certain contexts

--- sanity/loader/LiveVisualEditing.tsx
+++ sanity/loader/LiveVisualEditing.tsx
@@ -2,15 +2,22 @@
 
 import { useLiveMode } from "@sanity/react-loader";
 import { VisualEditing } from "next-sanity";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { client } from "@/sanity/lib/client";
 
-// Always enable stega in Live Mode
-const stegaClient = client.withConfig({ stega: true });
-
 export default function LiveVisualEditing() {
+  const [stegaClient, setStegaClient] = useState(
+    client.withConfig({ stega: true }),
+  );
   useLiveMode({ client: stegaClient });
+
+  useEffect(() => {
+    // If not an iframe or a Vercel Preview deployment, turn off Stega
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== "preview" && window === parent) {
+      setStegaClient(client.withConfig({ stega: false }));
+    }
+  }, []);
 
   return <VisualEditing />;
 }
