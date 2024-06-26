5bd3be6c5e5ecff916419cd11bd3288ca837171b
George Sutton <46813782+georgeosutton@users.noreply.github.com>
2024-06-26 15:37:08 +0100

    fix: textMedia fragment add text resolver

--- sanity/lib/fragments/textMedia.ts
+++ sanity/lib/fragments/textMedia.ts
@@ -1,5 +1,7 @@
 import { IMAGE } from "@/sanity/lib/fragments/image";
 
+import { PORTABLE_TEXT } from "./portableText";
+
 export const TEXT_MEDIA = `//groq
 content[]{
     (_type == "image")=>{
@@ -9,7 +11,7 @@ content[]{
        _type,
        _key,
        text[]{
-        ...,
+        ${PORTABLE_TEXT}
        },
     },
  },
