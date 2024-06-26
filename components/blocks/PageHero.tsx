f088da73534e3eb543ed4abfb1d0d9d90253bd37
George Sutton <46813782+georgeosutton@users.noreply.github.com>
2024-06-26 16:39:00 +0100

    chore(lint): remove whitespace (new feature)

--- components/blocks/PageHero.tsx
+++ components/blocks/PageHero.tsx
@@ -10,7 +10,7 @@ const components: PortableTextComponents = {
   block: {
     h1: ({ children }) => {
       return (
-        <h1 className="px-1 text-center text-6xlHeading text-white md:text-7xlHeading ">
+        <h1 className="px-1 text-center text-6xlHeading text-white md:text-7xlHeading">
           {children}
         </h1>
       );
@@ -27,7 +27,7 @@ export default function PageHero(props: PageHeroBlock) {
           <PortableText value={text} components={components} />
         </div>
       )}
-      <div className="relative z-10 col-start-1 row-start-1 size-full bg-darkGradientHorizonal md:bg-darkGradientVertical " />
+      <div className="relative z-10 col-start-1 row-start-1 size-full bg-darkGradientHorizonal md:bg-darkGradientVertical" />
       {images && (
         <div className="col-start-1 row-start-1 grid md:grid-cols-2">
           {images.map((imageData) => {
