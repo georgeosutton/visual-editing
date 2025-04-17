b94696d06ff625e0f1c43886d4a8fcb13136b134
George Sutton <46813782+georgeosutton@users.noreply.github.com>
2025-04-17 14:56:26 +0100

    feat: add baseURL util

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getBaseUrl = () => {
+  if (process.env.VERCEL_ENV === "production") {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+  if (process.env.VERCEL_ENV === "preview") {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return "http://localhost:3000";
+};
